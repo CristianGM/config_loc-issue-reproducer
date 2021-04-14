0c23468b9e150df3ee0845b8c61a9313871482ee
cristian-garcia <cristian.garcia@scmspain.com>
2021-04-14 15:00:24 +0200

    Travis and scans

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,12 @@
+plugins {
+    id("com.gradle.enterprise") version "3.6.1"
+}
+
 rootProject.name = "config_loc-issue-reproducer"
 
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
